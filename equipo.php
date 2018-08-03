b51cfd0d8a29cc57953b342793e4a9ed260dfc64
Pamela <papereyra@gmail.com>
2018-08-03 15:27:55 -0300

    equipo falta arreglar css

---
+++ equipo.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<head>
+	<title>SEMEX ARGENTINA</title>
+	<?php require_once("meta.html"); ?>
+</head>
+<body  id="wrapper_equipo">
+<div id="wrapper">
+  <?php require_once("header.php"); ?>
+</div>
+<div class="contenedor_todoequipo">
+ <object data="equipo_central.html" style="width:100%; height:700px"></object>
+</div>
+
+<?php require_once("scripts.html"); ?>
+<?php require_once("footer.php"); ?>
+</body>
+</html>
