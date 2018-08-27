86165da2815c10b72e76e11d67b41de9eb337af2
Pamela <papereyra@gmail.com>
2018-08-27 13:49:50 -0300

    fix destacados deps

--- ficha_carne.php
+++ ficha_carne.php
@@ -157,27 +157,38 @@
         <div class="contenedor_deps_linea2">
           <div class="deps_fila1">DEP</div>
           <div class="deps_separador_columnas"></div>
-          <div class="deps_destacado">{{tc.toro.pruebas.dep_fp}}</div>
+          <div ng-class="{'deps_destacado': tc.toro.pruebas.dep_fp_flag == '1', 
+                          'deps_columnas2': !tc.toro.pruebas.dep_fp_flag || tc.toro.pruebas.dep_fp_flag == '0'}">{{tc.toro.pruebas.dep_fp}}</div>
           <div class="deps_separador_columnas"></div>
-          <div class="deps_columnas2">{{tc.toro.pruebas.dep_pn}}</div>
+          <div ng-class="{'deps_destacado': tc.toro.pruebas.dep_pn_flag == '1', 
+                          'deps_columnas2': !tc.toro.pruebas.dep_pn_flag || tc.toro.pruebas.dep_pn_flag == '0'}">{{tc.toro.pruebas.dep_pn}}</div>
           <div class="deps_separador_columnas"></div>
-          <div class="deps_columnas2">{{tc.toro.pruebas.dep_pd}}</div>
+          <div ng-class="{'deps_destacado': tc.toro.pruebas.dep_pd_flag == '1', 
+                          'deps_columnas2': !tc.toro.pruebas.dep_pd_flag || tc.toro.pruebas.dep_pd_flag == '0'}">{{tc.toro.pruebas.dep_pd}}</div>
           <div class="deps_separador_columnas"></div>
-          <div class="deps_columnas2">{{tc.toro.pruebas.dep_pf}}</div>
+          <div ng-class="{'deps_destacado': tc.toro.pruebas.dep_pf_flag == '1', 
+                          'deps_columnas2': !tc.toro.pruebas.dep_pf_flag || tc.toro.pruebas.dep_pf_flag == '0'}">{{tc.toro.pruebas.dep_pf}}</div>
           <div class="deps_separador_columnas"></div>
-          <div class="deps_columnas2">{{tc.toro.pruebas.dep_leche}}</div>
+          <div ng-class="{'deps_destacado': tc.toro.pruebas.dep_leche_flag == '1', 
+                          'deps_columnas2': tc.toro.pruebas.dep_leche_flag == '0'}">{{tc.toro.pruebas.dep_leche}}</div>
           <div class="deps_separador_columnas"></div>
-          <div class="deps_columnas2">{{tc.toro.pruebas.dep_cm}}</div>
+          <div ng-class="{'deps_destacado': tc.toro.pruebas.dep_cm_flag == '1', 
+                          'deps_columnas2': !tc.toro.pruebas.dep_cm_flag || tc.toro.pruebas.dep_cm_flag == '0'}">{{tc.toro.pruebas.dep_cm}}</div>
           <div class="deps_separador_columnas"></div>
-          <div class="deps_columnas2">{{tc.toro.pruebas.dep_gd}}</div>
+          <div ng-class="{'deps_destacado': tc.toro.pruebas.dep_gd_flag == '1', 
+                          'deps_columnas2': !tc.toro.pruebas.dep_gd_flag || tc.toro.pruebas.dep_gd_flag == '0'}">{{tc.toro.pruebas.dep_gd}}</div>
           <div class="deps_separador_columnas"></div>
-          <div class="deps_columnas2">{{tc.toro.pruebas.dep_marm}}</div>
+          <div ng-class="{'deps_destacado': tc.toro.pruebas.dep_marm_flag == '1', 
+                          'deps_columnas2': !tc.toro.pruebas.dep_marm_flag || tc.toro.pruebas.dep_marm_flag == '0'}">{{tc.toro.pruebas.dep_marm}}</div>
           <div class="deps_separador_columnas"></div>
-          <div class="deps_destacado">{{tc.toro.pruebas.dep_aob}}</div>
+          <div ng-class="{'deps_destacado': tc.toro.pruebas.dep_aob_flag == '1', 
+                          'deps_columnas2': !tc.toro.pruebas.dep_aob_flag || tc.toro.pruebas.dep_aob_flag == '0'}">{{tc.toro.pruebas.dep_aob}}</div>
           <div class="deps_separador_columnas"></div>
-          <div class="deps_columnas2">{{tc.toro.pruebas.dep_ce}}</div>
+          <div ng-class="{'deps_destacado': tc.toro.pruebas.dep_ce_flag == '1', 
+                          'deps_columnas2': !tc.toro.pruebas.dep_ce_flag || tc.toro.pruebas.dep_ce_flag == '0'}">{{tc.toro.pruebas.dep_ce}}</div>
           <div class="deps_separador_columnas"></div>
-          <div class="deps_columnas2">{{tc.toro.pruebas.dep_doc}}</div>
+          <div ng-class="{'deps_destacado': tc.toro.pruebas.dep_doc_flag == '1', 
+                          'deps_columnas2': !tc.toro.pruebas.dep_doc_flag || tc.toro.pruebas.dep_doc_flag == '0'}">{{tc.toro.pruebas.dep_doc}}</div>
         </div>
         <div class="contenedor_deps_linea2">
           <div class="deps_fila1">PREC%</div>
@@ -207,27 +218,38 @@
         <div class="contenedor_deps_linea2">
           <div class="deps_fila1">RANKING</div>
           <div class="deps_separador_columnas"></div>
-          <div class="deps_destacado">{{tc.toro.pruebas.rk_fp}}</div>
+          <div ng-class="{'deps_destacado': tc.toro.pruebas.dep_fp_flag == '1', 
+                          'deps_columnas2': !tc.toro.pruebas.dep_fp_flag || tc.toro.pruebas.dep_fp_flag == '0'}">{{tc.toro.pruebas.rk_fp}}</div>
           <div class="deps_separador_columnas"></div>
-          <div class="deps_columnas2">{{tc.toro.pruebas.rk_pn}}</div>
+          <div ng-class="{'deps_destacado': tc.toro.pruebas.dep_pn_flag == '1', 
+                          'deps_columnas2': !tc.toro.pruebas.dep_pn_flag || tc.toro.pruebas.dep_pn_flag == '0'}">{{tc.toro.pruebas.rk_pn}}</div>
           <div class="deps_separador_columnas"></div>
-          <div class="deps_columnas2">{{tc.toro.pruebas.rk_pd}}</div>
+          <div ng-class="{'deps_destacado': tc.toro.pruebas.dep_pd_flag == '1', 
+                          'deps_columnas2': !tc.toro.pruebas.dep_pd_flag || tc.toro.pruebas.dep_pd_flag == '0'}">{{tc.toro.pruebas.rk_pd}}</div>
           <div class="deps_separador_columnas"></div>
-          <div class="deps_columnas2">{{tc.toro.pruebas.rk_pf}}</div>
+          <div ng-class="{'deps_destacado': tc.toro.pruebas.dep_pf_flag == '1', 
+                          'deps_columnas2': !tc.toro.pruebas.dep_pf_flag || tc.toro.pruebas.dep_pf_flag == '0'}">{{tc.toro.pruebas.rk_pf}}</div>
           <div class="deps_separador_columnas"></div>
-          <div class="deps_columnas2">{{tc.toro.pruebas.rk_leche}}</div>
+          <div ng-class="{'deps_destacado': tc.toro.pruebas.dep_leche_flag == '1', 
+                          'deps_columnas2': !tc.toro.pruebas.dep_leche_flag || tc.toro.pruebas.dep_leche_flag == '0'}">{{tc.toro.pruebas.rk_leche}}</div>
           <div class="deps_separador_columnas"></div>
-          <div class="deps_columnas2">{{tc.toro.pruebas.rk_cm}}</div>
+          <div ng-class="{'deps_destacado': tc.toro.pruebas.dep_cm_flag == '1', 
+                          'deps_columnas2': !tc.toro.pruebas.dep_cm_flag || tc.toro.pruebas.dep_cm_flag == '0'}">{{tc.toro.pruebas.rk_cm}}</div>
           <div class="deps_separador_columnas"></div>
-          <div class="deps_columnas2">{{tc.toro.pruebas.rk_gd}}</div>
+          <div ng-class="{'deps_destacado': tc.toro.pruebas.dep_gd_flag == '1', 
+                          'deps_columnas2': !tc.toro.pruebas.dep_gd_flag || tc.toro.pruebas.dep_gd_flag == '0'}">{{tc.toro.pruebas.rk_gd}}</div>
           <div class="deps_separador_columnas"></div>
-          <div class="deps_columnas2">{{tc.toro.pruebas.rk_marm}}</div>
+          <div ng-class="{'deps_destacado': tc.toro.pruebas.dep_marm_flag == '1', 
+                          'deps_columnas2': !tc.toro.pruebas.dep_marm_flag || tc.toro.pruebas.dep_marm_flag == '0'}">{{tc.toro.pruebas.rk_marm}}</div>
           <div class="deps_separador_columnas"></div>
-          <div class="deps_destacado">{{tc.toro.pruebas.rk_aob}}</div>
+          <div ng-class="{'deps_destacado': tc.toro.pruebas.dep_aob_flag == '1', 
+                          'deps_columnas2': !tc.toro.pruebas.dep_aob_flag || tc.toro.pruebas.dep_aob_flag == '0'}">{{tc.toro.pruebas.rk_aob}}</div>
           <div class="deps_separador_columnas"></div>
-          <div class="deps_columnas2">{{tc.toro.pruebas.rk_ce}}</div>
+          <div ng-class="{'deps_destacado': tc.toro.pruebas.dep_rk_flag == '1', 
+                          'deps_columnas2': !tc.toro.pruebas.dep_rk_flag || tc.toro.pruebas.dep_rk_flag == '0'}">{{tc.toro.pruebas.rk_ce}}</div>
           <div class="deps_separador_columnas"></div>
-          <div class="deps_columnas2">{{tc.toro.pruebas.rk_doc}}</div></div>
+          <div ng-class="{'deps_destacado': tc.toro.pruebas.dep_doc_flag == '1', 
+                          'deps_columnas2': !tc.toro.pruebas.dep_doc_flag || tc.toro.pruebas.dep_doc_flag == '0'}">{{tc.toro.pruebas.rk_doc}}</div></div>
         <div class="contenedor_deps_linea2">
           <div class="deps_fila1">PROMEDIO</div>
           <div class="deps_separador_columnas"></div>
